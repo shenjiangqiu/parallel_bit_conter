13031521ed373c7d096ca655885497673f24c5d6
Jiangqiu Shen <ivy22233qiu@live.com>
2021-07-29 07:06:41 -0400

    update

---
+++ src/main/scala/parallel_bitcounter/generate.scala
@@ -0,0 +1,10 @@
+package parallel_bitcounter
+import chisel3.stage.ChiselGeneratorAnnotation
+
+object Generate {
+  def main(args: Array[String]): Unit = {
+    (new chisel3.stage.ChiselStage).execute(
+      Array("-X", "verilog"),
+      Seq(ChiselGeneratorAnnotation(() => new ParallelBitCounter())))
+  }
+}
